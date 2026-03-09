1130f14d5f1c58035b926e1113c2c94931849caf
Gift <kgift0825@gmail.com>
2026-03-09 11:20:34 +0100

    Implement CVE report generation with translations and document formatting
    Added functionality to create a CVE security report in both English and Spanish, including translations and document formatting.

--- automation.py
+++ automation.py
@@ -1,5 +1,18 @@
-import requests
-import time
+#pip install python-docx -> to install the document packages for python.
+#pip install deep_translator -> to install the translation packages
+
+from docx import Document #imports the Document class from the python-docx library, which is used to create and manipulate Word documents in Python.
+import requests #imports the requests library, which is used to make HTTP requests in Python.
+import os
+import platform
+from datetime import datetime
+from deep_translator import GoogleTranslator
+
+now = datetime.now()
+todays_date = now.strftime("%d:%m:%Y")
+
+translator=GoogleTranslator(source='en',target='es')
+
 def cve_search(cve_id):
   response=requests.get(f"https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={cve_id}")
 
@@ -9,7 +22,7 @@ def cve_search(cve_id):
     if not data.get("vulnerabilities"):
         return None, None, None # No vulnerabilities found for this CVE ID
 
-    cve_data = data["vulnerabilities"][0]["cve"]
+    cve_data = data["vulnerabilities"][0]["cve"] #gives the first item in the list
     description = cve_data["descriptions"][0]["value"]
     references = cve_data["references"]
 
@@ -42,8 +55,62 @@ elif cvss_score < 9.0: # CVSS v3.1: 7.0-8.9 High
 else: # CVSS v3.1: 9.0-10.0 Critical
   result = "Critical"
 
-dic={"Description":description,
+dic={"Date Created": todays_date,
+    "CVE Detail":cve_id,
+    "Description":description,
      "References":references,
      "Metric":[cvss_score,result]}
 
-print(dic)
+severity_map = {
+    "Low": "Bajo",
+    "Medium": "Medio",
+    "High": "Alto",
+    "Critical": "Crítico",
+    "Unknown": "Desconocido"
+}
+
+resultado_es = severity_map.get(result, "Desconocido")
+
+docx=Document() #Initialises the document
+docx.add_heading("CVE SECURITY REPORT",level=1)
+docx.add_heading(dic["CVE Detail"],1)
+
+docx.add_heading("Date Created",2)
+docx.add_paragraph(dic["Date Created"])
+
+docx.add_heading("Description",2)
+docx.add_paragraph(dic["Description"])
+
+docx.add_heading("References",2)
+for item in dic["References"]:
+  docx.add_paragraph(str(item),style="List Bullet") #Put the listed items into a bullet points
+  
+
+docx.add_heading("Metric",2)
+metric_line=f"{cvss_score} ({result})"
+docx.add_paragraph(metric_line,style="List Bullet") 
+
+docx.add_page_break() #creates a page break seperating the En version from the Es version, making for a better legibility
+docx.add_heading("INFORME DE SEGURIDAD DE CVE")
+docx.add_heading("Fecha Creada",2)
+docx.add_paragraph(dic["Date Created"])
+
+docx.add_heading("Descripción",level=2)
+docx.add_paragraph(translator.translate(dic["Description"])) 
+
+docx.add_heading("Referencias",2)
+for item in dic["References"]:
+  docx.add_paragraph(str(item),style="List Bullet")
+  
+
+docx.add_heading("Métricas",2)
+metric_line=f"{cvss_score} ({resultado_es})"
+docx.add_paragraph(metric_line,style="List Bullet")
+
+docx.save(f"{cve_id}.docx")
+print("The CVE document has been initialised and saved!")
+
+if platform.system() == "Windows":
+  os.startfile(f"{cve_id}.docx") #This opens the document created
+else:
+  os.system(f"open {cve_id}.docx")
